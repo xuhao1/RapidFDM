93c3b7918c0db68de358b52a3ed966db6a1db162
xuhao <me@xuhao1.com>
2016-12-14 21:14:20 +0800

    Re-factor Simulation

--- modules/simulation/src/simulation_dji_a3.cpp
+++ modules/simulation/src/simulation_dji_a3.cpp
@@ -6,25 +6,18 @@
 
 #define C_EARTH 6378137.0f
 
-#define MIXER_VTOL
-//#define MIXER_GENERAL
 
-namespace RapidFDM
-{
-    namespace Simulation
-    {
-        simulation_dji_a3_adapter::simulation_dji_a3_adapter(AircraftNode *aircraft) :
-                interval(5)
-        {
-            this->aircraft = aircraft;
-
+namespace RapidFDM {
+    namespace Simulation {
+        simulation_dji_a3_adapter::simulation_dji_a3_adapter(SimulatorAircraft *_sim_air) :
+                simulation_hil_adapter(_sim_air) {
             root_uri = "ws://127.0.0.1:19870/general";
             sim_uri = "ws://127.0.0.1:19870/controller/simulator/";
 
             c_root.set_access_channels(websocketpp::log::alevel::none);
             c_root.clear_access_channels(websocketpp::log::alevel::all);
 
-            c_root.init_asio();
+            c_root.init_asio(&io_service);
 
             c_root.set_message_handler(bind(&simulation_dji_a3_adapter::on_message_root, this, &c_root, ::_1, ::_2));
             c_root.set_fail_handler(bind(&simulation_dji_a3_adapter::on_assitant_failed, this, &c_root, ::_1));
@@ -34,38 +27,30 @@ namespace RapidFDM
 
             try_connect_assistant();
 
-            timer = new boost::asio::deadline_timer(c_root.get_io_service(), interval);
 
-            timer->async_wait([&](const boost::system::error_code &) {
-                this->tick();
-            });
-
-            intial_lati = 22.5416 * M_PI /180.0;
-            intial_lon = 113.8973 * M_PI /180.0;
+            intial_lati = 22.5416 * M_PI / 180.0;
+            intial_lon = 113.8973 * M_PI / 180.0;
 
         }
 
-        void simulation_dji_a3_adapter::on_message_root(client *c, websocketpp::connection_hdl hdl, message_ptr msg)
-        {
+        void simulation_dji_a3_adapter::on_message_root(client *c, websocketpp::connection_hdl hdl, message_ptr msg) {
 
             rapidjson::Document d;
             d.Parse(msg->get_payload().c_str());
-            if (!d.IsObject())
-            {
+            if (!d.IsObject()) {
                 std::wcerr << "Parse a3 assiant document failed!" << std::endl;
                 std::cout << msg->get_payload();
                 return;
             }
 
-            if( !d.HasMember("FILE") || !d["FILE"].IsString() || !d.HasMember("EVENT")) {
+            if (!d.HasMember("FILE") || !d["FILE"].IsString() || !d.HasMember("EVENT")) {
                 return;
             }
 
-            if(std::string(d["EVENT"].GetString()) != "device_arrival")
-            {
+            if (std::string(d["EVENT"].GetString()) != "device_arrival") {
                 std::cout << msg->get_payload() << std::endl;
                 return;
-            } else{
+            } else {
                 printf("Start connect simulator\n");
             }
 
@@ -73,16 +58,14 @@ namespace RapidFDM
             try_connect_simulator();
         }
 
-        void simulation_dji_a3_adapter::try_connect_simulator()
-        {
+        void simulation_dji_a3_adapter::try_connect_simulator() {
             std::cout << "Try to connect simulator on " << file_name << std::endl;
 
             std::string uri = sim_uri + file_name;
             websocketpp::lib::error_code ec;
             std::cout << uri << std::endl;
-            if(c_simulator != nullptr)
-            {
-                c_simulator->close(sim_connection_hdl,websocketpp::close::status::normal,"Close connect");
+            if (c_simulator != nullptr) {
+                c_simulator->close(sim_connection_hdl, websocketpp::close::status::normal, "Close connect");
             }
             c_simulator = new client;
             c_simulator->set_access_channels(websocketpp::log::alevel::none);
@@ -110,8 +93,7 @@ namespace RapidFDM
 
         }
 
-        void simulation_dji_a3_adapter::on_simulator_link_open(client *c, websocketpp::connection_hdl hdl)
-        {
+        void simulation_dji_a3_adapter::on_simulator_link_open(client *c, websocketpp::connection_hdl hdl) {
             rapidjson::Document d;
             d.SetObject();
             d.AddMember("SEQ", "FUCK", d.GetAllocator());
@@ -125,29 +107,21 @@ namespace RapidFDM
             const char *str = json_to_buffer(d, size);
             c->send(hdl, str, size, websocketpp::frame::opcode::text);
 
-            printf("Simulator %s Open",file_name.c_str());
+            printf("Simulator %s Open", file_name.c_str());
 
         }
 
-        void simulation_dji_a3_adapter::tick()
-        {
-
-            timer->expires_at(timer->expires_at() + interval);
-
-            if (total_tick_count ++ % 200 == 0) {
+        void simulation_dji_a3_adapter::tick_func() {
+            if (total_tick_count++ % 200 == 0) {
                 try_connect_assistant();
-                printf("time: %f d tick %d\n",total_tick_count/200.0, dcount);
+                printf("time: %f d tick %d\n", total_tick_count / 200.0, dcount);
                 dcount = 0;
             }
-
-            timer->async_wait([&](const boost::system::error_code &) {
-                this->tick();
-            });
+            this->send_realtime_data();
         }
 
         void simulation_dji_a3_adapter::on_message_simulator(client *c, websocketpp::connection_hdl hdl,
-                                                             message_ptr msg)
-        {
+                                                             message_ptr msg) {
             rapidjson::Document d;
             d.Parse(msg->get_payload().c_str());
 
@@ -160,74 +134,35 @@ namespace RapidFDM
                 RcR = fast_value(d, "RcR");
                 RcT = fast_value(d, "RcT");
                 return;
-            }
-            else if (fast_string(d, "EVENT") == "sim_pwm") {
+            } else if (fast_string(d, "EVENT") == "sim_pwm") {
                 /*
                  * stdout: {
-    "EVENT": "sim_pwm",
-    "channels": [....    ],
-    "tick": 60
+                   "EVENT": "sim_pwm",
+                   "channels": [....    ],
+                    "tick": 60
                  */
 
-                if (total_tick_count % 200 == 0)
-                {
-                    printf("tick latency %ld sim:%ld fc %ld\n",this->simulator_tick - (int)(fast_value(d,"tick")),
-						this->simulator_tick ,(int)(fast_value(d, "tick"))
-					);
+                if (total_tick_count % 200 == 0) {
+                    printf("tick latency %ld sim:%ld fc %ld\n", this->simulator_tick - (int) (fast_value(d, "tick")),
+                           this->simulator_tick, (int) (fast_value(d, "tick"))
+                    );
                 }
+
                 for (int chn = 0; chn < 8; chn++) {
                     pwm[chn] = d["channels"][chn].GetDouble();
-                    pwm[chn] = (pwm[chn] / 10000 - 0.5)*2;
-                    pwm[chn] = float_constrain(pwm[chn],-1,1);
+                    pwm[chn] = (pwm[chn] / 10000 - 0.5) * 2;
+                    pwm[chn] = float_constrain(pwm[chn], -1, 1);
                 }
-
-#ifdef MIXER_GENERAL
-                aircraft->set_control_value("main_engine_0/thrust", (pwm[0] + 1) * 0.5);
-                aircraft->set_control_value("main_engine_1/thrust", (pwm[1] + 1) * 0.5);
-                aircraft->set_control_value("main_wing_0/flap_0",  pwm[2]);
-                aircraft->set_control_value("main_wing_0/flap_1", - pwm[5]);
-                aircraft->set_control_value("horizon_wing_0/flap_0", pwm[3]);
-                aircraft->set_control_value("horizon_wing_0/flap_1", pwm[3]);
-                aircraft->set_control_value("vertical_wing_0/flap", pwm[4]);
-
-//                aircraft->set_control_value("main_engine_0/thrust", (pwm[0] + 1) * 0.5);
-//                aircraft->set_control_value("main_wing_0/flap_0", - pwm[1]);
-//                aircraft->set_control_value("main_wing_0/flap_1", + pwm[1]);
-//                aircraft->set_control_value("horizon_wing_0/flap_0", -pwm[2]);
-//                aircraft->set_control_value("horizon_wing_0/flap_1", -pwm[2]);
-//                aircraft->set_control_value("vertical_wing_0/flap", pwm[3]);
-#endif
-
-#ifdef MIXER_FLYINGWING
-                aircraft->set_control_value("main_engine_0/thrust", (pwm[0] + 1) * 0.5);
-                aircraft->set_control_value("main_wing_0/flap_0",  pwm[2]);
-                aircraft->set_control_value("main_wing_0/flap_1", - pwm[3]);
-                //aircraft->set_control_value("horizon_wing_0/flap_0", pwm[3]);
-                //aircraft->set_control_value("horizon_wing_0/flap_1", pwm[3]);
-                aircraft->set_control_value("vertical_wing_0/flap", pwm[4]);
-#endif
-
-#ifdef MIXER_VTOL
-                aircraft->set_control_value("main_engine_0/thrust", (pwm[0] + 1) * 0.5);
-                aircraft->set_control_value("main_engine_1/thrust", (pwm[1] + 1) * 0.5);
-                aircraft->set_control_value("main_engine_2/thrust", (pwm[2] + 1) * 0.5);
-                aircraft->set_control_value("main_engine_3/thrust", (pwm[3] + 1) * 0.5);
-                aircraft->set_control_value("main_engine_5/thrust", (pwm[4] + 1) * 0.5);
-                aircraft->set_control_value("main_wing_0/flap_0",  + pwm[5]);
-                aircraft->set_control_value("main_wing_0/flap_1", - pwm[6]);
-
-#endif
+                on_pwm_data_receieve(pwm, 8);
             }
 
-            if (on_receive_pwm!= nullptr)
-            {
+            if (on_receive_pwm != nullptr) {
                 (*on_receive_pwm)();
             }
             dcount++;
         }
 
-        void simulation_dji_a3_adapter::add_values(rapidjson::Document &d)
-        {
+        void simulation_dji_a3_adapter::push_json_to_app(rapidjson::Document &d) {
             rapidjson::Value a3_value(rapidjson::kObjectType);
             add_value(a3_value, motor_starter, d, "motor_started");
             add_value(a3_value, RcA, d, "RcA");
@@ -244,10 +179,10 @@ namespace RapidFDM
 
             a3_value.AddMember("PWM", pwm_array, d.GetAllocator());
             d.AddMember("a3_sim_status", a3_value, d.GetAllocator());
+
         }
 
-        void simulation_dji_a3_adapter::on_simulator_link_failed(client *c, websocketpp::connection_hdl hdl)
-        {
+        void simulation_dji_a3_adapter::on_simulator_link_failed(client *c, websocketpp::connection_hdl hdl) {
             std::cerr << "A3 simulator failed, will try later" << std::endl;
             this->sim_online = false;
         }
@@ -257,22 +192,18 @@ namespace RapidFDM
             this->sim_online = false;
         }
 
-        void simulation_dji_a3_adapter::on_assitant_open(client *c, websocketpp::connection_hdl hdl)
-        {
+        void simulation_dji_a3_adapter::on_assitant_open(client *c, websocketpp::connection_hdl hdl) {
             std::cout << "Connect to A3 successfully" << std::endl;
             this->assiant_online = true;
         }
 
-        void simulation_dji_a3_adapter::on_assitant_failed(client *c, websocketpp::connection_hdl hdl)
-        {
+        void simulation_dji_a3_adapter::on_assitant_failed(client *c, websocketpp::connection_hdl hdl) {
             this->assiant_online = false;
             std::cerr << "A3 Assitant failed or disconnected, will try later" << std::endl;
         }
 
-        void simulation_dji_a3_adapter::send_realtime_data()
-        {
-            if(!this->sim_online)
-            {
+        void simulation_dji_a3_adapter::send_realtime_data() {
+            if (!this->sim_online) {
                 return;
             }
             rapidjson::Document d;
@@ -282,31 +213,31 @@ namespace RapidFDM
             Eigen::Quaterniond convert = (Eigen::Quaterniond) Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitY());
             //TODO:Fix convention
 
-            Eigen::Vector3d pos = (Eigen::Vector3d) aircraft->get_ground_transform().translation();
+            Eigen::Vector3d pos = (Eigen::Vector3d) aircraftNode->get_ground_transform().translation();
             pos = convert * pos;
 
-            d.AddMember("tick", (int64_t)simulator_tick, d.GetAllocator());
-            d.AddMember("lati", intial_lati  + pos.x()/C_EARTH, d.GetAllocator());
-            d.AddMember("lon",intial_lon + pos.y()/ C_EARTH  / cos(intial_lati + pos.x()/C_EARTH), d.GetAllocator());
-            d.AddMember("height",-  pos.z(), d.GetAllocator());
+            d.AddMember("tick", (int64_t) simulator_tick, d.GetAllocator());
+            d.AddMember("lati", intial_lati + pos.x() / C_EARTH, d.GetAllocator());
+            d.AddMember("lon", intial_lon + pos.y() / C_EARTH / cos(intial_lati + pos.x() / C_EARTH), d.GetAllocator());
+            d.AddMember("height", -pos.z(), d.GetAllocator());
             d.AddMember("rho", 1.29f, d.GetAllocator());
 
             AirState airState;
-            ComponentData data = aircraft->get_component_data();
+            ComponentData data = aircraftNode->get_component_data();
             add_value(d, data.get_airspeed_mag(airState), d, "airspeed");
 
             Eigen::Vector3d acc = sim_air->gAcc;
-            acc.z() = acc.z() -  9.81f;
+            acc.z() = acc.z() - 9.81f;
             acc = convert * acc;
             add_vector(d, acc, d, "acc");
 
-            Eigen::Vector3d vel = convert * aircraft->get_ground_velocity();
+            Eigen::Vector3d vel = convert * aircraftNode->get_ground_velocity();
             add_vector(d, vel, d, "vel");
 
-            add_vector(d, (convert * aircraft->get_angular_velocity()), d, "angular_vel");
+            add_vector(d, (convert * aircraftNode->get_angular_velocity()), d, "angular_vel");
 
             Eigen::Quaterniond quat =
-                    convert * (Eigen::Quaterniond) aircraft->get_ground_transform().rotation() * convert;
+                    convert * (Eigen::Quaterniond) aircraftNode->get_ground_transform().rotation() * convert;
 
             add_attitude(d, quat, d, "q");
 
@@ -317,8 +248,7 @@ namespace RapidFDM
 
         }
 
-        void simulation_dji_a3_adapter::try_connect_assistant()
-        {
+        void simulation_dji_a3_adapter::try_connect_assistant() {
             if (!assiant_online) {
                 websocketpp::lib::error_code ec;
                 client::connection_ptr con = c_root.get_connection(root_uri, ec);
@@ -330,6 +260,14 @@ namespace RapidFDM
                 c_root.connect(con);
             }
 
+        }
+
+        bool simulation_dji_a3_adapter::enable_simulation() {
+            return (this->motor_starter && this->assiant_online);
+        }
+
+        void simulation_dji_a3_adapter::update_before_sim(long tick) {
+            this->simulator_tick = tick;
         }
     }
 }
